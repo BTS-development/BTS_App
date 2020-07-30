f408b4536701de041f76dba70addd7541abd505f
structsize <52183019+structsize@users.noreply.github.com>
2020-07-30 22:44:26 +0900

    [UPDATE] update file name

--- BluetoothTemperatureSubmitter/app/src/main/java/com/example/bluetoothtemperaturesubmitter/SignupActivity.kt
+++ BluetoothTemperatureSubmitter/app/src/main/java/com/example/bluetoothtemperaturesubmitter/SignupActivity.kt
@@ -3,9 +3,9 @@ package com.example.bluetoothtemperaturesubmitter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class SigninActivity : AppCompatActivity() {
+class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signin)
+        setContentView(R.layout.activity_signup)
     }
 }
