1f811fac1a539dde17a448a65e9099dbcc55a7b8
kjw12qwas <49026547+kjw12qwas@users.noreply.github.com>
2020-08-02 03:47:11 +0900

    [CREATE] MainPage_UI

---
+++ BluetoothTemperatureSubmitter/app/src/main/java/com/example/bluetoothtemperaturesubmitter/MainPageActivity.kt
@@ -0,0 +1,11 @@
+package com.example.bluetoothtemperaturesubmitter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_page)
+    }
+}
