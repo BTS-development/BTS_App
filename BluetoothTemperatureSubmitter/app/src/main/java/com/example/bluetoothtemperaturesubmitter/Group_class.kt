44c84f8972c1330541e9e7e98354ac7a8f258756
kjw12qwas <49026547+kjw12qwas@users.noreply.github.com>
2020-08-03 16:37:05 +0900

    [CREATE] Group_management

---
+++ BluetoothTemperatureSubmitter/app/src/main/java/com/example/bluetoothtemperaturesubmitter/Group_class.kt
@@ -0,0 +1,41 @@
+package com.example.bluetoothtemperaturesubmitter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+
+class Group_class(val context: Context, val group_list: ArrayList<group>) : BaseAdapter(){
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val view: View = LayoutInflater.from(context).inflate(R.layout.group_management,null)
+
+        val group_image = view.findViewById<ImageView>(R.id.group_image)
+        val group_name = view.findViewById<TextView>(R.id.group_name)
+        val group_people = view.findViewById<TextView>(R.id.group_people)
+
+
+        val group = group_list[position]
+        val resourceId = context.resources.getIdentifier(group.image, "drawable", context.packageName)
+        group_image.setImageResource(resourceId)
+        group_name.text = group.name
+        group_people.text = group.people
+
+        return view
+    }
+
+    override fun getItem(position: Int): Any {
+        return group_list[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return 0
+    }
+
+    override fun getCount(): Int {
+        return group_list.size
+    }
+
+}
---
+++ BluetoothTemperatureSubmitter/app/src/main/java/com/example/bluetoothtemperaturesubmitter/group.kt
@@ -0,0 +1,3 @@
+package com.example.bluetoothtemperaturesubmitter
+
+class group(val image: String, val name: String, val people: String)
