008018ff1cddf7a8831d923ab588168aa816c3ca
kjw12qwas <49026547+kjw12qwas@users.noreply.github.com>
2020-08-19 10:34:47 +0900

    [Create] Fragment Ui

--- BluetoothTemperatureSubmitter/app/src/main/java/com/example/bluetoothtemperaturesubmitter/MainPageActivity.kt
+++ BluetoothTemperatureSubmitter/app/src/main/java/com/example/bluetoothtemperaturesubmitter/MainPageActivity.kt
@@ -2,10 +2,20 @@ package com.example.bluetoothtemperaturesubmitter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toolbar
+import com.example.bluetoothtemperaturesubmitter.Fragment.Main_User
+import kotlinx.android.synthetic.main.activity_main_page.*
 
 class MainPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_page)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_frame,Main_User())
+            .commit()
+
     }
+
 }
