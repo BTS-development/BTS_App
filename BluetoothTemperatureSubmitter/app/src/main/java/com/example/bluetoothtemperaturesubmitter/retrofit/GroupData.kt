273893d570819d93eef1b90add20d53dfdd93453
structsize <52183019+structsize@users.noreply.github.com>
2020-08-19 13:29:48 +0900

    Revert "[UPDATE] create retrofit method"
    This reverts commit b5a78f3a2a613251fd3ec4cbed7b951df6f53795.

--- BluetoothTemperatureSubmitter/app/src/main/java/com/example/bluetoothtemperaturesubmitter/retrofit/GroupData.kt
+++
@@ -1,9 +0,0 @@
-package com.example.bluetoothtemperaturesubmitter.retrofit
-
-data class GroupData(
-    val id : String,
-    val name : String,
-    val code : String,
-    val created_at : String,
-    val owner : String
-)
