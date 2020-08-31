c221bd37d96f0af6da64e102e96b978b6647d291
kjw12qwas <49026547+kjw12qwas@users.noreply.github.com>
2020-08-31 23:01:31 +0900

    [Create] Group_create

---
+++ BluetoothTemperatureSubmitter/app/src/main/java/com/example/bluetoothtemperaturesubmitter/group/Group_create.kt
@@ -0,0 +1,12 @@
+package com.example.bluetoothtemperaturesubmitter.group
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bluetoothtemperaturesubmitter.R
+
+class Group_create : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_group_create)
+    }
+}
