4c7ac011ae5b58e182c940c949f8e77ae8dbca8b
kjw12qwas <49026547+kjw12qwas@users.noreply.github.com>
2020-09-16 14:42:02 +0900

    [UPDATE] 메인 화면 로그인 화면 회원가입 화면 수정

--- BluetoothTemperatureSubmitter/app/src/main/java/com/example/bluetoothtemperaturesubmitter/MainActivity.kt
+++ BluetoothTemperatureSubmitter/app/src/main/java/com/example/bluetoothtemperaturesubmitter/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.bluetoothtemperaturesubmitter
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnSignIn.setOnClickListener {
+            val intent = Intent(this@MainActivity, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        btnSignUp.setOnClickListener {
+            val intent = Intent(this@MainActivity, SignupActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
